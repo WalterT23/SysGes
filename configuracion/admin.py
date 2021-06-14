6da0438a9a296870afdda30cd79d823a62171304
Jessica <>
2021-06-14 18:21:10 -0400

    Mod. en el modulo de configuracion
    Se realizaron modificaciones en el modulo de configuracion

--- configuracion/admin.py
+++ configuracion/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Configuracion,lineaBase
 # Register your models here.
+
+
+#Dentro del parentesis va el nombre de la clase que se creo en models
+
+#para agregar los campos en modo lectura
+class ConfiguracionAdmin(admin.ModelAdmin):
+    readonly_fields=('create','updated')
+    
+class lineaBaseAdmin(admin.ModelAdmin):
+    readonly_fields=('create','updated')
+    
+admin.site.register(Configuracion, ConfiguracionAdmin)
+admin.site.register(lineaBase, lineaBaseAdmin)
--- configuracion/views.py
+++ configuracion/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Configuracion
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url='/ingresar')
 def configuracion_view(request):
     #le decimos a django que importe todos los objetos de la clase configuracion
-    configuracion = configuracion.objects.all()
+    configuracion = Configuracion.objects.all()
     return render(request,"configuracion/configuracion_tpl.html",{'configuracion':configuracion})
+
+
+
 
